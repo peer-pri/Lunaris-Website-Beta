8859ee06ccd2f7cfe3f11751ec2754e9ca0a1895
EinzzCookie <109176503+peer-pri@users.noreply.github.com>
2023-11-14 08:26:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
-// Add animation to the header
-const header = document.querySelector('header');
-header.addEventListener('mouseover', () => {
-    header.style.transform = 'scale(1.1)';
-});
-header.addEventListener('mouseout', () => {
-    header.style.transform = 'scale(1)';
-});
-
-// Add animation to the download links
-const downloadLinks = document.querySelectorAll('a');
-downloadLinks.forEach(link => {
-    link.addEventListener('mouseover', () => {
-        link.style.color = '#ffcc00';
-    });
-    link.addEventListener('mouseout', () => {
-        link.style.color = '#fff';
-    });
+document.getElementById("downloadButton").addEventListener("click", function() {
+    // Code to handle file download
+    alert("File downloaded successfully!");
 });
